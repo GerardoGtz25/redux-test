5d46f561fe5ecafdf1059841bb8e894c5b12b838
Gerardo <ge.gut5@gmail.com>
2019-11-08 01:34:48 -0600

    PubicacionesActions

--- src/components/Publicaciones/Publicaciones.js
+++ src/components/Publicaciones/Publicaciones.js
@@ -2,27 +2,61 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import * as usuariosActions from '../../actions/usuariosActions'
+import * as publicacionesActions from './../../actions/publicacionesActions'
+
+const { traerTodos: usuariosTraerTodos } = usuariosActions
+const { traerTodos: publicacionesTraerTodos } = publicacionesActions
 
 class Publicaciones extends Component {
   
   componentDidMount() {
-    console.log(this.props)
-    if (!this.props.usuarios.length) {
-      this.props.traerTodos()
+    if (!this.props.usuariosReducer.usuarios.length) {
+      this.props.usuariosTraerTodos()
+      this.props.publicacionesTraerTodos()
     }
   }
 
+  renderPublicaciones() {
+    return this.props.publicacionesReducer.publicaciones.map(publicacion => {
+      return (
+        <div className="row" key={ publicacion.id }>
+          <div className="col-1">
+           { publicacion.id }
+          </div>
+          <div className="col-11">
+            { publicacion.title }
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
+    console.log(this.props)
     return (
-      <div className="col-12">
-        <h1>Publicaciones de { this.props.match.params.key }</h1>
+      <div>
+        <div className="col-12">
+          <h1>Publicaciones de { this.props.match.params.key }</h1>
+        </div>
+        {
+         this.renderPublicaciones()
+        }
       </div>
+      
     )
   }
 }
 
-const mapStateToProps = (reducers) => {
-  return reducers.usuariosReducer
+const mapStateToProps = ({ usuariosReducer, publicacionesReducer }) => {
+  return {
+    usuariosReducer,
+    publicacionesReducer
+  }
 }
 
-export default connect(mapStateToProps, usuariosActions)(Publicaciones)
+const mapDispatchToProps = {
+  usuariosTraerTodos,
+  publicacionesTraerTodos
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones)
